8041cff622589326786e84efa2f426620d9fc8b0
Mustafa SAĞIROĞLU <mustafasagiroglu0645@gmail.com>
2023-07-09 13:32:49 +0300

    09/07/2023 new study

---
+++ src/test/java/Homeworks_Assigments_LMS/Q09.java
@@ -0,0 +1,22 @@
+package Homeworks_Assigments_LMS;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class Q09 extends TestBase {
+    @Test
+    public void test01() {
+        //https://www.selenium.dev/downloads/ adresine gidelim
+        //Sayfanın resmini alalım
+        //Platforms Supported by Selenium altındaki Browsers bölümü görünene kadar sayfayı indirelim
+        //Browser bölümünden Chrome driver bölümündeki documentation linkine tıklıyalım
+        //Documentation webelementinin resmini alalım
+        //All versions available in Downloads altında Latest stable release olan linki tıklayalım
+        //Açılan pencerede chromedriver'i indirelim
+        //Driver'in indiğini doğrulayalım
+        //İndirmiş olduğumuz dosyayı silelim
+        //Silindiğini doğrulayalım
+        //Not: Bu task'in her adımı için Html rapor oluşturalım(Extent Report)
+
+    }
+}
